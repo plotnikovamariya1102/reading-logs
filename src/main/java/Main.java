65d0eab625e715adc483dddcbf074f87a793ba7e
Plotnikova Mariya <m.buryakova@yandex.ru>
2021-01-28 08:26:22 +0500

    System.out.println added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,8 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
-    long expected = 30;
 
-    long actual = service.calculate(amount, registered);
-
-    boolean passed = expected == actual;
-
-    System.out.println(passed);
+    System.out.println(service.calculate(amount, registered));
   }
 }
 
